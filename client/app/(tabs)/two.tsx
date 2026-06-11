ae8bcc457cbe05e064912c2f011e245f75039f47
newyorkdeep <mpp14021972@gmail.com>
2026-06-11 16:37:03 +0300

    api key input

--- client/app/(tabs)/two.tsx
+++ client/app/(tabs)/two.tsx
@@ -1,16 +1,43 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { ActivityIndicator, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 import { Themes } from '../../themes';
 import { getTheme, saveTheme } from '../../themes_logic'; 
 import { Picker } from '@react-native-picker/picker';
 import { DeviceEventEmitter } from 'react-native';
+import { Link } from 'expo-router';
+
+interface KeyResponse {
+  keyFound: boolean,
+  prefix: string,
+}
 
 export default function TabTwoScreen() {
   const [selectedTheme, setSelectedTheme] = useState<keyof typeof Themes>("light");
+  const [apiKey, setApiKey] = useState<KeyResponse | null>(null);
+  const [newApiKey, setNewApiKey] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [refreshTrigger, setRefreshTrigger] = useState<boolean>(false);
+  const KEY_URL = 'http://localhost:8000/update_key';
+
+
+  
+  useEffect(() => {
+    fetch('http://localhost:8000/check_api')
+    .then((response) => response.json())
+    .then((json) => {
+      setApiKey(json);
+      setLoading(false);
+    })
+    .catch((error) => {
+      console.error('Error fetching api key', error);
+      setLoading(false);
+    });
+  }, [refreshTrigger]);
 
   const handleThemeChange = async (itemValue: keyof typeof Themes) => {
+
     // Update local UI immediately
     setSelectedTheme(itemValue); 
     
@@ -20,6 +47,37 @@ export default function TabTwoScreen() {
     // Shout to everyone else (including the Tab Bar!)
     DeviceEventEmitter.emit('themeChanged', itemValue); 
   };
+
+  const handleKeySave = async() => {
+    if (!newApiKey.trim()) {
+      console.error('Key cannot be empty')
+      return;
+    }
+    setLoading(true);
+    try {
+      const response = await fetch(KEY_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },  
+        body: JSON.stringify({api_key: newApiKey}),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setRefreshTrigger(prev => !prev); 
+        setNewApiKey('');
+      } else {
+        console.error('Error', data.detail || 'Something went wrong'); 
+      } 
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
 
   const colors = Themes[selectedTheme];
 
@@ -48,6 +106,19 @@ export default function TabTwoScreen() {
       borderRadius: 15,
     }
   });
+
+  if (loading) {
+      return (
+        <View style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: colors.background,
+        }}>
+          <ActivityIndicator size="small" color="#000000" />
+        </View>
+      );
+  }
   
   return (
     <View style={styles.container}>
@@ -67,6 +138,40 @@ export default function TabTwoScreen() {
           </Picker.Item>
         ))}
       </Picker>
+      <>
+        <View style={{ height: 10 }} />
+        <Text style={{ color: colors.text, marginBottom: 10 }}>Api key:</Text>
+        
+        {apiKey && apiKey.keyFound ? (
+          /* Safer object lookup notation matching how you accessed 'key found' */
+          <Text>{apiKey.prefix}</Text>
+        ) : (
+          <Text>Key not found</Text>
+        )}
+      </>
+      <View style={{ height: 10 }} />
+      <TextInput
+        placeholder="Enter new API Key"
+        placeholderTextColor="#999"
+        value={newApiKey}
+        onChangeText={setNewApiKey}
+        // secureTextEntry={true}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <View style={{ height: 10 }} />
+      <TouchableOpacity  
+        onPress={handleKeySave}
+        disabled={loading}
+      >
+        {loading ? (
+          <ActivityIndicator color="#fff" />
+        ) : (
+          <Text>Save Key</Text>
+        )}
+      </TouchableOpacity>
+      <View style={{ height: 10 }} />
+      <Link href='https://app.exchangerate-api.com/sign-up' target="_blank" rel="noopener noreferrer">Click here to obtain a free key</Link>
     </View>
   );
 }
